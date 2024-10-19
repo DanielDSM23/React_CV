8168de1287bdc17e91f73d9a1f803731d4383e20
Daniel MONTEIRO <101821091+DanielDSM23@users.noreply.github.com>
2024-10-19 22:17:56 +0200

    patch: env reading

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ function Login() {
         }}
         onSubmit={async (values) => {
           try {
-            const response = await fetch(`${process.env.API_URL}/api/auth/login`, {
+            const response = await fetch(`http://${import.meta.env.VITE_API_URL}/auth/login`, {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json'
